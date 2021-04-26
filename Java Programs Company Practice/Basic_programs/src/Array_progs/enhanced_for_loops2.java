682c2d734bc19601c18721dcd3218806a1fb4239
srj <37172427+asg-suraj@users.noreply.github.com>
2021-04-26 09:53:36 +0530

    Update enhanced_for_loops2.java

--- Java Programs Company Practice/Basic_programs/src/Array_progs/enhanced_for_loops2.java
+++ Java Programs Company Practice/Basic_programs/src/Array_progs/enhanced_for_loops2.java
@@ -20,7 +20,7 @@ public class enhanced_for_loops2 {
 	public static void main(String[] args) {
 		// Here we are using Enhanced For loop
 
-		String names[] = { "dhanshri", "Sarika", "Chinmay", "Sanket", "Pragati", "Madhuri" };
+		String names[] = { "jagjit", "priyanka", "Chanky", "Sanket", "Prakash", "Madhav" };
 		
 		for (String name : names) { //see how it is declared 
 			System.out.println(name); //it will print all names of names array of String
